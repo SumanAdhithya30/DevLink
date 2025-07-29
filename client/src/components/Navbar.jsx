fd70e08e94e851768a5b06a36bc85822ecd34195
SumanAdhithya30 <sumanadhithya701@gmail.com>
2025-07-29 23:29:15 +0530

    Modified Navbar

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -2,10 +2,10 @@ import { Link } from "react-router-dom";
 
 const Navbar = () => {
     return (
-        <nav className = "bg-blue-600 text-white p-4 flex justify-between">
+        <nav className = "bg-indigo-600 text-white p-4 flex justify-between">
             <div className = "font-bold text-xl ">DevLink</div>
             <div className="flex gap-4">
-                <Link to="/" className="hover:underline">Home</Link>
+                <Link to="/" className="hover:underline ">Home</Link>
                 <Link to="/login" className="hover:underline">Login</Link>
                 <Link to="/register" className="hover:underline">Register</Link>
                 <Link to="/dashboard" className="hover:underline">Dashboard</Link>
