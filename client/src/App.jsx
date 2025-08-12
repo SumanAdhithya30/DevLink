e4a0c3c5d460a75d60ab264dcfb21388df3e0d5d
SumanAdhithya30 <sumanadhithya701@gmail.com>
2025-08-12 23:44:25 +0530

    modified app.jsx

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,37 +1,79 @@
-import { BrowserRouter as Router,Routes, Route } from "react-router-dom";
+import React from "react";
+import { BrowserRouter as Router, Routes, Route, useLocation } from "react-router-dom";
+import Navbar from "./components/Navbar";
 import Home from "./pages/Home";
 import Login from "./pages/Login";
 import Register from "./pages/Register";
 import Dashboard from "./pages/Dashboard";
-import Navbar from "./components/Navbar";
-import PrivateRoute from "./components/PrivateRoute";
 import Developers from "./pages/Developers";
 
-function App(){
+function AppContent() {
+  const location = useLocation();
+  const hideNavbarOn = ["/login", "/register"]; // pages without navbar
+
+  return (
+    <>
+      {/* Conditionally render Navbar */}
+      {!hideNavbarOn.includes(location.pathname) && <Navbar />}
+
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/register" element={<Register />} />
+
+        {/* Dashboard routes */}
+        <Route path="/dashboard" element={<Dashboard />}>
+          <Route path="developers" element={<Developers />} />
+        </Route>
+      </Routes>
+    </>
+  );
+}
+
+export default function App() {
   return (
     <Router>
-        <Navbar />
-          <Routes>
-            <Route path ="/" element={<Home />}/>
-            <Route path="/login" element={<Login />}/>
-            <Route path= "/register" element={<Register />}/>
-            <Route 
-              path="/dashboard" 
-              element={
-                <PrivateRoute>
-                  <Dashboard />
-                </PrivateRoute>}/>
-            <Route
-              path="/dashboard/developers"
-              element={
-                <PrivateRoute>
-                  <Developers />
-                </PrivateRoute>
-              }
-            />
-          </Routes>
+      <AppContent />
     </Router>
   );
 }
 
-export default App;
+
+
+// import { BrowserRouter as Router,Routes, Route } from "react-router-dom";
+// import Home from "./pages/Home";
+// import Login from "./pages/Login";
+// import Register from "./pages/Register";
+// import Dashboard from "./pages/Dashboard";
+// import Navbar from "./components/Navbar";
+// import PrivateRoute from "./components/PrivateRoute";
+// import Developers from "./pages/Developers";
+
+// function App(){
+//   return (
+//     <Router>
+//         <Navbar />
+//           <Routes>
+//             <Route path ="/" element={<Home />}/>
+//             <Route path="/login" element={<Login />}/>
+//             <Route path= "/register" element={<Register />}/>
+//             <Route 
+//               path="/dashboard" 
+//               element={
+//                 <PrivateRoute>
+//                   <Dashboard />
+//                 </PrivateRoute>}/>
+//             <Route
+//               path="/dashboard/developers"
+//               element={
+//                 <PrivateRoute>
+//                   <Developers />
+//                 </PrivateRoute>
+//               }
+//             />
+//           </Routes>
+//     </Router>
+//   );
+// }
+
+// export default App;
